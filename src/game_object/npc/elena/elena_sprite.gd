6c4a7368fde9af2c5fe803e91bf3c259886f7163
Maximillian-520 <maximilliankenas520@gmail.com>
2026-09-02 22:57:46 +0700

    Add character legs are below when sitting

--- src/game_object/npc/elena/elena_sprite.gd
+++ src/game_object/npc/elena/elena_sprite.gd
@@ -7,13 +7,29 @@ class_name ElenaSprite
 @export var interact_animation_name : String = "interact"
 @export var sit_animation_name : String = "sit"
 
+@export_subgroup("Z Index Settings")
+@export var normal_z_index : int = 1
+@export var sitting_z_index : int = 0
+
+# ==================================================================================================
+#                Virtual methods
+# ==================================================================================================
+func _ready() -> void:
+	z_index = normal_z_index
+
 # ==================================================================================================
 #                Sprite methods
 # ==================================================================================================
-func do_idle(): play(idle_animation_name)
+func do_idle():
+	play(idle_animation_name)
+	z_index = normal_z_index
 
-func do_walk(): play(walk_animation_name)
+func do_walk():
+	play(walk_animation_name)
+	z_index = normal_z_index
 
 func do_interact(): play(interact_animation_name)
 
-func do_sit(): play(sit_animation_name)
+func do_sit():
+	play(sit_animation_name)
+	z_index = sitting_z_index
