085fcea6aaf99f9408520ddad7ba8d9ed0c7326d
Maximillian-520 <maximilliankenas520@gmail.com>
2026-09-08 11:54:52 +0700

    Added pause

---
+++ src/ui/component/button/main_button.gd
@@ -0,0 +1,64 @@
+extends Button
+class_name MainButton
+
+@export_subgroup("References")
+@export var normal_texture_rect : TextureRect
+@export var hover_texture_rect : TextureRect
+@export var press_texture_rect : TextureRect
+@export var text_label : Label
+
+@export_subgroup("Button Settings")
+@export var normal_text_color : Color = Color.WHITE
+@export var hover_text_color : Color = Color.BLACK
+@export var press_text_color : Color = Color.BLACK
+
+# ==================================================================================================
+#                Virtual methods
+# ==================================================================================================
+func _ready() -> void:
+	# Assertion check
+	assert(normal_texture_rect, "normal_texture_rect is missing")
+	assert(hover_texture_rect, "hover_texture_rect is missing")
+	assert(press_texture_rect, "press_texture_rect is missing")
+	assert(text_label, "text_label is missing")
+	# Connect signals
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+	button_down.connect(_on_button_down)
+	button_up.connect(_on_button_up)
+	# Initialize
+	button_normal()
+
+# ==================================================================================================
+#                Button methods
+# ==================================================================================================
+func button_normal():
+	normal_texture_rect.show()
+	hover_texture_rect.hide()
+	press_texture_rect.hide()
+	text_label.modulate = normal_text_color
+
+func button_hovered():
+	normal_texture_rect.hide()
+	hover_texture_rect.show()
+	press_texture_rect.hide()
+	text_label.modulate = hover_text_color
+
+func button_pressed():
+	normal_texture_rect.hide()
+	hover_texture_rect.hide()
+	press_texture_rect.show()
+	text_label.modulate = press_text_color
+
+# ==================================================================================================
+#                Signal listener methods
+# ==================================================================================================
+func _on_mouse_entered() -> void: button_hovered()
+
+func _on_mouse_exited() -> void: button_normal()
+
+func _on_button_down() -> void: button_pressed()
+
+func _on_button_up() -> void:
+	if is_hovered(): button_hovered()
+	else: button_normal()
---
+++ src/ui/pause/pause_ui.gd
@@ -0,0 +1,54 @@
+extends Control
+class_name PauseUI
+
+@export_subgroup("References")
+@export var resume_button : Button
+@export var quit_button : Button
+
+@export_subgroup("Pause Settings")
+@export var quit_scene_path : String
+
+# ==================================================================================================
+#                Virtual methods
+# ==================================================================================================
+func _ready() -> void:
+	# Assertion check
+	assert(resume_button, "resume_button is missing")
+	assert(quit_button, "quit_button is missing")
+	# Connect signals
+	resume_button.pressed.connect(_on_resume_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
+	# Initialize
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	hide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		toggle_pause()
+		get_viewport().set_input_as_handled()
+
+# ==================================================================================================
+#                Pause methods
+# ==================================================================================================
+func toggle_pause() -> void:
+	if get_tree().paused: _unpause()
+	else: _pause()
+
+func _pause() -> void:
+	visible = true
+	get_tree().paused = true
+
+func _unpause() -> void:
+	visible = false
+	get_tree().paused = false
+
+func _quit_game() -> void:
+	get_tree().paused = false
+	#get_tree().change_scene_to_file(quit_scene_path)
+
+# ==================================================================================================
+#                Signal listener methods
+# ==================================================================================================
+func _on_resume_pressed() -> void: _unpause()
+
+func _on_quit_pressed() -> void: _quit_game()
