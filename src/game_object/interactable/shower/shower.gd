1947846cba713d025bc546650c12b09907ab6d02
Maximillian-520 <maximilliankenas520@gmail.com>
2026-09-10 23:47:11 +0700

    Added UI block to shower

--- src/game_object/interactable/shower/shower.gd
+++ src/game_object/interactable/shower/shower.gd
@@ -5,6 +5,7 @@ class_name Shower
 @export var normal_sprite : Sprite2D
 @export var locked_sprite : Sprite2D
 @export var lock_collision : CollisionShape2D
+@export var ui_block : Control
 
 @export_subgroup("Audio Settings")
 @export var open_sfx_name : String = "shower_open"
@@ -19,10 +20,12 @@ func _ready() -> void:
 	assert(normal_sprite, "normal_sprite is missing")
 	assert(locked_sprite, "locked_sprite is missing")
 	assert(lock_collision, "lock_collision is missing")
+	assert(ui_block, "ui_block is missing")
 	# Initialize
 	normal_sprite.show()
 	locked_sprite.hide()
 	lock_collision.disabled = true
+	ui_block.hide()
 
 # ==================================================================================================
 #                Stove methods
@@ -31,6 +34,7 @@ func lock():
 	normal_sprite.hide()
 	locked_sprite.show()
 	lock_collision.disabled = false
+	ui_block.show()
 	AudioManager.play_sfx(open_sfx_name)
 	AudioManager.play_sound(showering_sound_name)
 
@@ -38,5 +42,6 @@ func unlock():
 	normal_sprite.show()
 	locked_sprite.hide()
 	lock_collision.disabled = true
+	ui_block.hide()
 	AudioManager.play_sfx(close_sfx_name)
 	AudioManager.stop_sound(showering_sound_name)
