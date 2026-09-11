bc616ad7c46eede796b5044c72424234ea58a1c8
Maximillian-520 <maximilliankenas520@gmail.com>
2026-09-11 21:08:26 +0700

    Added temp ending

--- src/game_object/scene/ending_scene.gd
+++ src/game_object/scene/ending_scene.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 const MAIN_MENU_SCENE_PATH : String = "res://src/scene/main_menu_scene.tscn"
 
+@onready var animation : AnimationPlayer = $Animation
+
 func _ready() -> void:
-	await get_tree().create_timer(1).timeout
+	await animation.animation_finished
 	SaveManager.set_value("has_ended_game", false)
 	SaveManager.save_game()
 	get_tree().change_scene_to_file(MAIN_MENU_SCENE_PATH)
